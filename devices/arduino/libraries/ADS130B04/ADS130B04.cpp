a6d656e72048771e9ce18e2457940e94406616c3
bedakno <s6beknop@uni-bonn.de>
2023-10-12 20:39:50 +0200

    ENH: improved functionality of ADC library

--- devices/arduino/libraries/ADS130B04/ADS130B04.cpp
+++ devices/arduino/libraries/ADS130B04/ADS130B04.cpp
@@ -6,61 +6,81 @@ SPISettings LowPowerMode(4092000, MSBFIRST, SPI_MODE1);
 SPISettings VeryLowPowerMode(2048000, MSBFIRST, SPI_MODE1);
 SPISettings SPIsetting;
 
-struct PINS{
-        uint8_t CS;
-        uint8_t EOC;
-    };
 
-struct DATA{
-    uint16_t CMD_RESPONSE;
-    uint16_t CHx_VAL[4] = {0};
-    uint16_t REG_VAL[16] = {0};
-    bool REG_R;
-};  
+uint8_t CS;
+uint8_t EOC;
 
-PINS pins;
-DATA data;
+
+uint16_t CMD_RESPONSE;
+uint16_t CHx_VAL[4] = {0};
+uint16_t REG_VAL[16] = {0};
+
+
+struct _register{
+    uint8_t addr;
+    uint16_t content;
+};
+
+register ID;
+register STATUS;
+register MODE;
+register CLOCK;
+register GAIN;
+register GLOBAL_CHOP_CFG;
+register CH0_CFG;
+register CH1_CFG;
+register CH2_CFG;
+register CH3_CFG;
+register REG_CRC;
+
+ID.addr = 0x00;
+STATUS.addr = 0x01;
+MODE.addr = 0x02;
+CLOCK.addr = 0x03;
+GAIN.addr = 0x04;
+GLOBAL_CHOP_CFG.addr = 0x06;
+CH0_CFG.addr = 0x09;
+CH1_CFG.addr = 0x0E;
+CH2_CFG.addr = 0x13;
+CH3_CFG.addr = 0x18;
+REG_CRC.addr = 0x3E;
 
 
 ADS130B04::ADS130B04(uint8_t CS_PIN, uint8_t EOC_PIN){
-    pins.CS = CS_PIN;
-    pins.EOC = EOC_PIN;
-    data.REG_R = false;
+    CS = CS_PIN;
+    EOC = EOC_PIN;
+    CHx_VAL = {0};
+    //fill register contents
 }
 
 ADS130B04::~ADS130B04(){
 }
 
 void ADS130B04::null(){
-    command = 0b00;
-    query();
+    query(commands.null);
     transADC();
 }
 
 void ADS130B04::reset(){
-    command = 0b10001;
-    query();
+    query(commands.reset);
 
 }
 
 void ADS130B04::standby(){
     command = 0b100010;
-    query();
+    query(commands.standby);
 }
 
 void ADS130B04::wakeup(){
-    command = 0b110011;
-    query();
+    query(commands.wakeup);
 }
 
 void ADS130B04::lock(){
-    command = 0b10101010101;
-    query();
+    query(commands.lock);
 }
 
 void ADS130B04::unlock(){
-    command = 0b11001010101;
-    query();
+    query(commands.unlock);
 }
 
 int ADS130B04::rreg(uint16_t a, uint16_t n){
@@ -68,7 +88,7 @@ int ADS130B04::rreg(uint16_t a, uint16_t n){
         return -99;
     }
     command = 0b1010000000000000 + (a<<7) + n;
-    query();
+    query(command, );
     return 0;
 }
 
@@ -81,19 +101,19 @@ void ADS130B04::wreg(uint16_t n, uint16_t a){
 
 
 
-void ADS130B04::GAIN::increase(uint8_t channel){
-    rreg()
+void ADS130B04::increaseGain(uint8_t channel){
+    int a = rreg(0,0);
 }
 
-void ADS130B04::GAIN::decrease(uint8_t channel){
+void ADS130B04::decreaseGain(uint8_t channel){
     
 }
 
-void ADS130B04::GAIN::set(uint8_t channel, uint8_t gain){
+void ADS130B04::setGain(uint8_t channel, uint8_t gain){
     
 }
 
-void ADS130B04::GAIN::get(uint8_t channel){
+void ADS130B04::getGain(uint8_t channel){
     
 }
 
@@ -121,27 +141,23 @@ void ADS130B04::setPowerMode(uint8_t mode){
     //command = getclocksetting();
 }
 
-void ADS130B04::query(){
+void ADS130B04::query(uint16_t command, struct register *regs[], uint8_t n){
     SPI.beginTransaction(SPIsetting);
-    digitalWrite(pins.CS, LOW);
-    data.CMD_RESPONSE = SPI.transfer16(command);
-    uint8_t n_vals = 0;
-    if(data.REG_R){n_vals = n_reg;}
-    else n_vals = 4;
+    digitalWrite(CS, LOW);
+    STATUS.content = SPI.transfer16(command);
+    for(uint8_t i; i<4; i++){
+        if(i==0){CHx_VAL[i] = SPI.transfer16(gen_crc(command));}
+        else CHx_VAL[i] = SPI.transfer16(0x00);  
+    }
+    uint16_t crc = SPI.transfer16(0x00);  
+    digitalWrite(CS, HIGH);
 
-    uint16_t transmit = 0x00;
-    for(uint8_t i; i<n_vals; i++){
-        transmit = uint16_t((crc_mode&&i==0)*gen_crc(command));
-        if(data.REG_R){
-            data.REG_VAL[i] = SPI.transfer16(transmit);
-        }
-        else data.CHx_VAL[i] = SPI.transfer16(transmit);  
+    digitalWrite(CS, LOW);
+    for (uint8_t i = 0; i < n; i++){
+       regs->content = SPI.transfer16(0x00);
     }
 
     digitalWrite(pins.CS, HIGH);
-    if(command!=0x00){
-        data.REG_R = true;
-    }
 }
 
 uint16_t ADS130B04::gen_crc(uint16_t command){
--- devices/arduino/libraries/ADS130B04/ADS130B04.h
+++ devices/arduino/libraries/ADS130B04/ADS130B04.h
@@ -3,80 +3,52 @@
 
 #include <SPI.h>
 
+class ADS130B04;
+
+struct _register;
+typedef struct _register register;
+
 class ADS130B04{
 private:
-    uint16_t ADDRESS_LIM = 0b111111;
-    uint16_t NUMBER_LIM = 0b1111111;
-    uint8_t n_reg;
-    uint16_t command;
-    uint16_t crc;
-    uint8_t crc_mode = 0;
-    uint16_t ccitt_crc_poly = 0b0001000000100001;
-    uint16_t ansi_crc_poly = 0b1000000000000101;
+    const uint16_t adress_lim = 0b111111;
+    const uint16_t number_lim = 0b1111111;
 
-    void query();
-    uint16_t gen_crc(uint16_t);
+    struct{
+        const uint16_t null = 0b00;
+        const uint16_t reset = 0b10001;
+        const uint16_t standby = 0b100010;
+        const uint16_t wakeup = 0b110011;
+        const uint16_t lock = 0b10101010101;
+        const uint16_t unlock = 0b11001010101;
+    }commands;
+
+    uint8_t crc_mode = 0;
+    const uint16_t ccitt_crc_poly = 0b0001000000100001;
+    const uint16_t ansi_crc_poly = 0b1000000000000101;
+
+    void query(uint16_t, struct *register[], uint8_t);
+    uint16_t genCRC(uint16_t);
     void transADC();
     bool error_a_n(uint16_t, uint16_t);
     void setSPIsetting(uint8_t);
 
-
     /*device settings and status indicators (read-only registers)
     detailed description in datasheet: https://www.ti.com/lit/ds/symlink/ads130b04-q1.pdf?HQS=dis-mous-null-mousermode-dsf-pf-null-wwe&ts=1695742377424&ref_url=https%253A%252F%252Fwww.mouser.de%252F*/
-
-    struct ID{
-        uint16_t addr = 0x00;
-        uint16_t content;
-    };
-
-    struct STATUS{
-        uint16_t addr = 0x01;
-        uint16_t content;
-    };
-
-    struct MODE{
-        uint16_t addr = 0x02;
-        uint16_t content;
-    };
-
-    struct CLOCK{
-        uint16_t addr = 0x03;
-        uint16_t content;
-    };
-
-    struct GAIN{
-        uint16_t addr = 0x04;
-        uint16_t content;
-    };
-
-    struct GLOBAL_CHOP_CFG{
-        uint16_t addr = 0x06;
-        uint16_t content;
-    };
-
-    struct Chx_CFG{
-        uint16_t addr[4] = {0x09, 0x0E, 0x13, 0x18};
-        uint16_t content;
-    };
-
-    struct REG_CRC{
-        uint16_t addr = 0x3E;
-        uint16_t content;
-    };
     
      
 public:
+    uint16_t CHx_VAL[4];
+
     ADS130B04(uint8_t, uint8_t);
     ~ADS130B04();
     void SPIsetup(bool doubleslit);
     
     void setPowerMode(uint8_t);
-    struct GAIN{
-        void increase(uint8_t channel);
-        void decrease(uint8_t channel);
-        void set(uint8_t channel, uint8_t gain);
-        void get(uint8_t channel);
-    };
+   
+    void increaseGain(uint8_t channel);
+    void decreaseGain(uint8_t channel);
+    void setGain(uint8_t channel, uint8_t gain);
+    void getGain(uint8_t channel);
     
    
     void getADCVal();
@@ -86,8 +58,6 @@ public:
     void enableADC();
     void disableADC();
 
-
-
     void null();
     void reset();
     void standby();
@@ -96,11 +66,6 @@ public:
     void unlock();
     int rreg(uint16_t, uint16_t);
     void wreg(uint16_t, uint16_t);
-    
-
- 
-
-   
 
 };
 
